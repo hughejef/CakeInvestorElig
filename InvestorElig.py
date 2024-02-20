343a7cb0bc13ed1f7ef4f1608629b902b5c8a268
hughejef <hughejef@oregonstate.edu>
2024-02-19 18:44:03 -0800

    initial commit

---
+++ InvestorElig.py
@@ -0,0 +1,28 @@
+
+# Check Investor
+investor = "Sponge"
+
+
+# check FICO eligibility
+check_fico():
+
+
+# check LTV eligibilitty
+
+
+# check loan purpose eligibility
+
+
+# check property type eligibility
+
+
+# check loan amount eligibility
+
+
+# check DTI eligibility
+
+
+# check reserves eligibility
+
+
+if __name__ == "__main__":
